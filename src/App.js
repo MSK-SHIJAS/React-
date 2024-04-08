7a00e01f62ad2d098729fdf6e2fe4ce1a5511129
MSK-SHIJAS <mskshijas786@gmail.com>
2024-04-08 13:26:59 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './card.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="grid">
+      <div className="b">
+      <img src="1.jpg" />
+       <h3>ADAM JOHNSON</h3>
+       <h6>DEVELOPER</h6>
+       </div>
+       <div className="c">
+      <img src="1.jpg" />
+       <h3>ADAM JOHNSON</h3>
+       <h6>DEVELOPER</h6>
+       </div>
+         <div class="c">
+          <img src="1.jpg" />
+          <h3>LINDA LARSON</h3>
+          <h6>MANAGER</h6>
+            </div>
+            <div class="c">
+          <img src="1.jpg" />
+          <h3>LINDA LARSON</h3>
+          <h6>MANAGER</h6>
+            </div>
+            <div class="c">
+          <img src="1.jpg" />
+          <h3>LINDA LARSON</h3>
+          <h6>MANAGER</h6>
+            </div>
+            <div class="b">
+          <img src="1.jpg" />
+          <h3>LINDA LARSON</h3>
+          <h6>MANAGER</h6>
+            </div>
+            </div>     
+
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+    {/* // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
